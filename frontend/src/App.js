2f2de78be94685bb1d4f0dba7097ea389d7255c3
Shashank <shashank2003aj@outlook.com>
2024-07-10 18:02:50 +0530

    Free Hand Writing Addded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,91 @@ import { useEffect, useRef, useState } from 'react';
 
 function App() {
   const [gridState, setGridState] = useState(true);
-  const [scale, setScale] = useState("1x");
+  const [drawState, setDrawState] = useState(true);
+  const [scale, setScale] = useState(1);
+  const [isClear, setClear] = useState(true);
   const grid = useRef(0);
   const scaleRange = useRef(0);
   const gridButton = useRef(0);
+  const drawButton = useRef(0);
   const rangeBox = useRef(0);
   const range = useRef(0);
   const scaleInfo = useRef(0);
+  const pad = useRef(0);
+  const workSpace = useRef(0);
+  const clearButton = useRef(0);
+
+  var pen_color = "white";
+  var isGrid = false;
 
   useEffect(() => {
     const context = grid.current.getContext("2d");
+    const padContext = pad.current.getContext("2d");
+
+    var pointArray = [];
+    var interval = null;
+    var isWriting = false;
+
+    pad.current.addEventListener('mousedown', writeStart);
+    pad.current.addEventListener('mousemove', writing);
+    pad.current.addEventListener('mouseup', writeEnd);
+    pad.current.addEventListener('mouseout', writeEnd);
+
+    pad.current.addEventListener('touchstart', writeStart);
+    pad.current.addEventListener('touchmove', writing);
+    pad.current.addEventListener('touchend', writeEnd);
+
+    function writeStart(event)
+    {
+      event.preventDefault()
+      isWriting = true
+
+      padContext.lineWidth = 10;
+      padContext.strokeStyle = "white";
+      padContext.shadowColor = "white";
+      padContext.shadowBlur = 2;
+      padContext.lineJoin = "round";
+      padContext.lineCap = "round";
+    
+      padContext.beginPath()
+      pointArray.push(getWriteCoordinates(event))
+      padContext.moveTo(pointArray[0].x, pointArray[0].y)
+
+      interval = setInterval(render, 16)
+    }
+
+    function writing(event)
+    {
+      event.preventDefault()
+      if (isWriting)
+      {
+        pointArray.push(getWriteCoordinates(event))
+      }
+    }
+
+    function writeEnd(event)
+    {
+      event.preventDefault()
+      isWriting = false;
+      setClear(false);
+      setTimeout(() => {
+        clearInterval(interval)
+      }, 17)
+    }
+
+    function render()
+    {
+        while (pointArray.length > 0)
+        {
+          padContext.lineTo(pointArray[0].x, pointArray[0].y)
+          pointArray = pointArray.slice(1)
+        }
+  
+        padContext.stroke()
+    }
 
     function drawGrid(){
-      context.lineWidth = 0.03;
+      context.lineWidth = 1;
       
       for (var x = 50; x <= 2000; x += 50) {
           context.moveTo(x, 0);
@@ -26,12 +98,15 @@ function App() {
           context.moveTo(0, x);
           context.lineTo(2000, x);
       }
-      
-      context.strokeStyle = "white";
+
+      context.strokeStyle = "rgb(255, 255, 255, 0.05)";
       context.stroke();
     }
 
-    drawGrid();
+    if (!isGrid)
+    {
+      drawGrid();
+    }
   }, [])
 
   useEffect(() => {
@@ -43,13 +118,44 @@ function App() {
     else
     {
       grid.current.style.opacity = 1;
-      gridButton.current.style.backgroundColor = "rgba(0, 173, 0, 0.847)"
+      gridButton.current.style.backgroundColor = "white"
     }
   }, [gridState])
+
+  useEffect(() => {
+    if (!isClear)
+    {
+      clearButton.current.classList.add("active")
+    }
+    else
+    {
+      clearButton.current.classList.remove("active")
+    }
+  }, [isClear])
+
+  useEffect(() => {
+    if (!drawState)
+    {
+      pad.current.style.pointerEvents = "none";
+      workSpace.current.style.cursor = "grab";
+      drawButton.current.style.backgroundColor = "transparent";
+    }
+    else
+    {
+      pad.current.style.pointerEvents = "all";
+      workSpace.current.style.cursor = "crosshair"
+      drawButton.current.style.backgroundColor = "white"
+    }
+  }, [drawState])
 
   function gridToggle()
   {
     setGridState(!gridState);
+  }
+
+  function drawToggle()
+  {
+    setDrawState(!drawState);
   }
 
   function scaleToggle()
@@ -57,7 +163,7 @@ function App() {
     rangeBox.current.classList.toggle("active")
     if (rangeBox.current.className === "active")
     {
-      scaleRange.current.style.backgroundColor = "rgb(186, 0, 0)"
+      scaleRange.current.style.backgroundColor = "white"
     }
     else
     {
@@ -65,11 +171,30 @@ function App() {
     }
   }
 
+  function clear()
+  {
+    const ctx = pad.current.getContext("2d");
+    ctx.clearRect(0, 0, 2000, 2000)
+    setClear(true)
+  }
+
   function rangeUpdate()
   {
     let temp = 0.5 + (range.current.value / 100)
-    setScale(`${temp.toFixed(1)}x`)
+    setScale(temp)
     grid.current.style.transform = `scale(${temp})`
+    pad.current.style.transform = `scale(${temp})`
+  }
+
+  function getWriteCoordinates(event) {
+    const clientX = event.clientX || event.touches[0].clientX;
+    const clientY = event.clientY || event.touches[0].clientY;
+    const ws = workSpace.current
+    const s = 0.5 + (range.current.value / 100)
+    const canvasX = ((clientX - ws.offsetLeft + ws.scrollLeft) / s);
+    const canvasY = ((clientY - ws.offsetTop + ws.scrollTop) / s);
+    console.log(s)
+    return { x: canvasX, y: canvasY };
   }
 
   return (
@@ -77,11 +202,15 @@ function App() {
       <header>
         <h1>Math <span>NotePad</span></h1>
         <div id="tools">
+          <div className="Button" id="clearButton" ref={clearButton} onClick={clear}></div>
+          <div className="Button" id="undoButton"></div>
+          <div className="Button" id="redoButton"></div>
+          <div className="Button" id="drawButton" ref={drawButton} onClick={drawToggle}></div>
           <div className="Button" id="gridButton" ref={gridButton} onClick={gridToggle}></div>
           <div className="Button" id="scaleRange" ref={scaleRange} onClick={scaleToggle}>
             <div id="sRange" ref={rangeBox}>
               <input type={"range"} orient="vertical" ref={range} onInput={rangeUpdate}></input>
-              <p ref={scaleInfo}>{scale}</p>
+              <p ref={scaleInfo}>{`${scale.toFixed(1)}x`}</p>
             </div>
           </div>
         </div>
@@ -92,7 +221,8 @@ function App() {
 
       </div>
 
-      <div className='workSpace'>
+      <div className='workSpace' ref={workSpace}>
+        <canvas ref={pad} id="pad" width="2000" height="2000"></canvas>
         <canvas ref={grid} width="2000" height="2000"></canvas>
       </div>
     </div>
